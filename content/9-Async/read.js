3c8f22fd5b204f7ac6a89314148632db076a43f6
adriancuadros <adrian@reservamos.mx>
2018-05-28 23:30:29 -0500

    Almost done async read

--- content/9-Async/read.js
+++ content/9-Async/read.js
@@ -1,5 +1,24 @@
 // Read File System - Example of async
+import fs from 'fs';
+// What files are in the support folder ?
+fs.readdir(`${__dirname}/support/`, (err, files) => {
+  files.forEach((file) => {
+    require(`${__dirname}/support/${file}`);
+  });
+});
 // Request Http with "request" (without promises)
+import request from 'request';
+
+//Get request for a website
+request.get('https://www.google.com', (error, response) => {
+  console.log(response);
+});
+
+//Get request for an API
+const reservamosEndpoint = 'https://www.reservamos.mx/api/v1/cities.json?prefetch=true';
+request.get(reservamosEndpoint, (error, response) => {
+  console.log(response);
+});
 // Concept of a promise - Usefulness
 // Fetch - Create an http Request
 // setTimeout - Use a promise to exemplify database, report calculations
