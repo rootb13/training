d1f65cabbae4f454adbb40ff7ee21fd23a4f31e2
adriancuadros <adrian@reservamos.mx>
2018-05-29 22:32:20 -0500

    Promises read is done

--- content/9-Async/read.js
+++ content/9-Async/read.js
@@ -6,19 +6,56 @@ fs.readdir(`${__dirname}/support/`, (err, files) => {
     require(`${__dirname}/support/${file}`);
   });
 });
+
 // Request Http with "request" (without promises)
 import request from 'request';
 
 //Get request for a website
 request.get('https://www.google.com', (error, response) => {
-  console.log(response);
+  // Uncomment to see the html in google.com
+  // console.log(response);
 });
 
 //Get request for an API
 const reservamosEndpoint = 'https://www.reservamos.mx/api/v1/cities.json?prefetch=true';
 request.get(reservamosEndpoint, (error, response) => {
-  console.log(response);
+  const cities = JSON.parse(response.body);
+  console.log(`Respuesta tiene ${cities.length} ciudades`);
 });
-// Concept of a promise - Usefulness
+
+// Promises
 // Fetch - Create an http Request
-// setTimeout - Use a promise to exemplify database, report calculations
+import rp from 'request-promise';
+
+// Using then
+const requestPromise = rp.get(reservamosEndpoint);
+requestPromise.then((response) => {
+  const cities = JSON.parse(response);
+  console.log(`Respuesta tiene ${cities.length} ciudades`)
+});
+
+// Creating a custom promise
+const promise = new Promise((resolve) => {
+  console.log("Some expensive operation here");
+  setTimeout(() => resolve(), 3000);
+});
+promise.then(() => console.log("Operation done!"));
+
+// Functions and promises
+function reservamosCityNames () {
+  const reservamosCitiesPromise = rp.get(reservamosEndpoint);
+  return reservamosCitiesPromise.then((response) => {
+    return JSON.parse(response);
+  }).then((cities) => {
+    return cities.map((city) => {
+      return city.name;
+    });
+  });
+}
+
+// Using simpler functions
+reservamosCityNames().then((cityNames) => {
+  console.log(cityNames);
+});
+
+// Refactor function :) 
